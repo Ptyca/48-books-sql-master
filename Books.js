8097ffa2240e699507eb2961fd57c203214f1966
Ptyca <82829609+Ptyca@users.noreply.github.com>
2021-09-17 20:20:32 +0300

    books

--- Books.js
+++ Books.js
@@ -14,12 +14,11 @@ const Books = {};
  * @returns {Promise<string>} Tekstas, apibudinantis, koks autorius ir kurias metais isleido knyga.
  */
 Books.create = async (connection, authorId, bookName, bookReleaseYear) => {
-    const sql = 'INSERT INTO `books` \
-                    (`id`, `firstname`, `lastname`)\
-                     VALUES (NULL, "'+ authorFirstname + '", "' + authorLastname + '")';
-    const [rows] = await connection.execute(sql);
-    console.log(rows)
-    return `${rows[0].firstname} ${rows[0].lastname} sukure ${bookName} ${metais}!`;
+    const sql = 'INSERT INTO books (authorId,title, releaseYear)\
+    VALUES ("'+ authorId + '", "' + bookName + '", "' + bookReleaseYear + '")';
+    [rows] = await connection.execute(sql);
+    const createBook = `book added!: author with ID ${authorId}, book title "${bookName}", release year ${bookReleaseYear}.`
+    return createBook;
 }
 
 /**
@@ -28,6 +27,20 @@ Books.create = async (connection, authorId, bookName, bookReleaseYear) => {
  * @returns {Promise<Object[]>} Tekstas, apibudinantis, koks autorius ir kurias metais isleido knyga.
  */
 Books.listAll = async (connection) => {
+    const sql = 'SELECT * \
+        FROM `books`';
+    const [rows] = await connection.execute(sql);
+    console.log(rows)
+    if (rows.length === 0) {
+        return 'sarasas yra tuscias'
+    }
+    count = 0;
+    const infoList = [];
+    for (let { title, releaseYear } of rows) {
+        infoList.push(`${++count}) ${title} ${releaseYear}`)
+    };
+    const title = `Knygu sarasas:\n`;
+    return title + infoList.join(`\n`);
 }
 
 /**
@@ -37,24 +50,127 @@ Books.listAll = async (connection) => {
  * @returns {Promise<Object[]>} Sarasas su knygu objektais.
  */
 Books.findByName = async (connection, bookName) => {
+    const sql = 'SELECT * FROM `books` WHERE `title` LIKE "%' + bookName + '%"';
+    const [rows] = await connection.execute(sql);
+    console.log(rows)
+    if (rows.length === 0) {
+        return 'Nera tokios knygos'
+    }
+    else {
+        const names = rows.map(obj => obj.title + ' ' + obj.releaseYear);
+        return `Rastas knyga (pgl varda): ${names.join(', ')}.`;
+    }
 }
+
+/**
+ * Knygu paieska pagal metus
+ * @param {Object} connection Objektas, su kuriuo kvieciame duombazes mainpuliavimo metodus.
+ * @param {number} bookReleaseYear Knygos Leidimo metai
+ * @returns Knygu sarasas pagal metus
+ */
 
 Books.findByYear = async (connection, bookReleaseYear) => {
+    const sql = 'SELECT * FROM `books` WHERE `releaseYear` LIKE "%' + bookReleaseYear + '%"';
+    const [rows] = await connection.execute(sql);
+    if (rows.length === 0) {
+        return 'Nera tokios knygos'
+    }
+    else {
+        const names = rows.map(obj => obj.title + ' ' + obj.releaseYear);
+        return `Rastas knyga:(pagal metus) ${names.join(', ')}.`;
+    }
 }
 
+/**
+ * Atnaujinti pagal knygos ID
+ * @param {Object} connection Objektas, su kuriuo kvieciame duombazes mainpuliavimo metodus.
+ * @param {number} bookId KNygos ID
+ * @param {*} propertyName Atnaujinamas stulpelis
+ * @param {*} propertyValue Atnaujinto stulpelio pavadinimas
+ * @returns Atnaujintas pasirinktas knygos stulpelis
+ */
 Books.updateById = async (connection, bookId, propertyName, propertyValue) => {
+    const props = ['id', 'title', 'releaseYear'];
+    if (!props.includes(propertyName)) {
+        return 'BLOGIS, stulpelis nerastas';
+    }
+
+    const sql = 'UPDATE `books`\
+                     SET `' + propertyName + '` = "' + propertyValue + '"\
+                      WHERE `books`.`id` =' + bookId;
+    [rows] = await connection.execute(sql);
+    if (rows.changedRows === 0) {
+        return `Pagal duota ID - ${bookId} autorius nerastas, atnaujinti nepavyko!`;
+    } else {
+        return `Knygos duomenys (pagal ID) atnaujinti sekmingai!`;
+    }
 }
 
+/**
+ * Atnaujinti pagal knygos pavadinimas
+ * @param {Object} connection Objektas, su kuriuo kvieciame duombazes mainpuliavimo metodus.
+ * @param {number} bookId Knygos ID
+ * @param {string} bookName Knygos Pavadinimas
+ * @returns Atnaujina informacija pagal pavadinima
+ */
 Books.updateNameById = async (connection, bookId, bookName) => {
+
+    const sql = 'UPDATE books SET title = "' + bookName + '" WHERE books.id =' + bookId;
+    [rows] = await connection.execute(sql);
+    const updatedTitleById = `Book with ID ${bookId} has a new title now as "${bookName}."`
+    return updatedTitleById;
 }
 
+/**
+ * Atnaujinti knygos info pagal metus
+ * @param {Object} connection Objektas, su kuriuo kvieciame duombazes mainpuliavimo metodus.
+ * @param {number} bookId 
+ * @param {number} bookReleaseYear 
+ * @returns atnaujina knygos info pagal metus
+ */
 Books.updateYearById = async (connection, bookId, bookReleaseYear) => {
+    const sql = 'UPDATE books SET releaseYear = "' + bookReleaseYear + '" WHERE books.id =' + bookId;
+    [rows] = await connection.execute(sql);
+    const updatedTitleById = `Book with ID ${bookId} has a new release year "${bookReleaseYear}."`
+    return updatedTitleById;
 }
 
+/**
+ * Istrina knyga pagal ID
+ * @param {Object} connection Objektas, su kuriuo kvieciame duombazes mainpuliavimo metodus.
+ * @param {number} bookId Knygos ID
+ * @returns istrina knyga
+ */
 Books.delete = async (connection, bookId) => {
+    const sql = 'DELETE FROM `books`\
+                      WHERE `books`.`id` =' + bookId;
+    const [rows] = await connection.execute(sql);
+
+    if (rows.affectedRows === 0) {
+        return `Pagal duota ID - ${bookId} knyga nerasta, istrinti nepavyko!`;
+    } else {
+        return `knyga nurodytu KNYGOSID: ${bookId} istrinta sekmingai!`;
+    }
+
 }
 
+/**
+ * Istrinti visas autoriaus knygas pagal Autoriaus ID
+ * @param {Object} connection Objektas, su kuriuo kvieciame duombazes mainpuliavimo metodus.
+ * @param {number} authorId Autoriaus ID
+ * @returns istrina VISAS autoriaus knyga pagal autoriaus ID
+ */
 Books.deleteAllByAuthorId = async (connection, authorId) => {
+    const sql = 'DELETE FROM `books`\
+                      WHERE `books`.`authorId` =' + authorId;
+    const [rows] = await connection.execute(sql);
+
+    if (rows.affectedRows === 0) {
+        return `Pagal duota AUTORIAUS ID - ${authorId} knyga nerasta, istrinti nepavyko!`;
+    } else {
+        return `knyga nurodytu AUTORIAUS ID: ${authorId} istrintas sekmingai!`;
+    }
+
 }
 
 module.exports = Books;
--- index.js
+++ index.js
@@ -62,9 +62,52 @@ app.init = async () => {
     const knygaCreate = await Books.create(conn, 1, 'Taika ir Taika', 1222);
     console.log(knygaCreate);
 
+    const knygaCreate2 = await Books.create(conn, 2, 'Nieko nebus', 2020);
+    console.log(knygaCreate2);
 
+    const knygaCreate3 = await Books.create(conn, 3, 'Grybu karas', 2021);
+    console.log(knygaCreate3);
+
+    const knygaCreate4 = await Books.create(conn, 4, 'Gerkles skausmai', 2021);
+    console.log(knygaCreate4);
+    console.log('')
+
+    const sarasas1 = await Books.listAll(conn);
+    console.log(sarasas1);
+    console.log('');
+
+
+    const sarasaPagalVarda3 = await Books.findByName(conn, 'ka');
+    console.log(sarasaPagalVarda3);
+    console.log('');
+
+    const sarasaPagalMetus = await Books.findByYear(conn, 20);
+    console.log(sarasaPagalMetus);
+    console.log('');
+
+    const updPagalID1 = await Books.updateById(conn, 3, 'releaseYear', 77777);
+    console.log(updPagalID1);
+    console.log('');
+
+
+    const updPagalName = await Books.updateNameById(conn, 3, 33333333);
+    console.log(updPagalName);
+    console.log('');
+
+    const updYearPagalName = await Books.updateNameById(conn, 1, 334444433);
+    console.log(updYearPagalName);
+    console.log('');
+
+    const istrintiPagalID = await Books.delete(conn, 1);
+    console.log(istrintiPagalID);
+    console.log('');
+
+    const istrintiPagalKnygosID = await Books.deleteAllByAuthorId(conn, 2);
+    console.log(istrintiPagalKnygosID);
+    console.log('');
 
 }
+
 
 app.init();
 
